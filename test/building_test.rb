9c007018dda196648345ab1bc9b2e312b6563d72
PhilipDeFraties <philipjfry338@gmail.com>
2020-07-14 09:46:56 -0600

    Add test_it_has_no_units_by_default test.

--- test/building_test.rb
+++ test/building_test.rb
@@ -3,14 +3,22 @@ require 'minitest/pride'
 require './lib/renter'
 require './lib/apartment'
 require './lib/building'
+require 'pry'
 
-
-class BuildinbTest < MiniTest::Test
+class BuildingTest < MiniTest::Test
 
   def test_it_exists
     building = Building.new
 
     assert_instance_of Building, building
   end
+
+
+  def test_it_has_no_units_by_default
+    building = Building.new
+
+    assert_equal [], building.units
+  end
+
 
 end
