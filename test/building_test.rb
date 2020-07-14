3d45a95ac8571350ac907f84592a3f6d4b3c8220
PhilipDeFraties <philipjfry338@gmail.com>
2020-07-14 10:57:16 -0600

    add test_it_can_add_renters.

--- test/building_test.rb
+++ test/building_test.rb
@@ -32,5 +32,25 @@ class BuildingTest < MiniTest::Test
     assert_equal [unit1, unit2], building.units
   end
 
+  def test_it_has_no_renters_by_default
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+
+    assert_equal [], building.renters
+  end
+
+  def test_it_can_add_renters
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    unit2 = Apartment.new({number: "B2", monthly_rent: 999, bathrooms: 2, bedrooms: 2})
+    renter1 = Renter.new("Aurora")
+    building.add_unit(unit1)
+    unit1.add_renter(renter1)
+    building.add_unit(unit2)
+    unit1.add_renter(renter1)
+
+    assert_equal ["Aurora"], building.renters
+  end
 
 end
