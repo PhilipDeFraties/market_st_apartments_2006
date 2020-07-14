5413027fa1a5668b2563eec382e2dd9534ca4ab1
PhilipDeFraties <philipjfry338@gmail.com>
2020-07-14 09:35:45 -0600

    Add test_it_can_add_renter test.

--- test/apartment_test.rb
+++ test/apartment_test.rb
@@ -38,6 +38,16 @@ class ApartmentTest < MiniTest::Test
   def test_it_has_no_renter_by_default
     unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
 
-    assert_equal [], unit1.renter
+    assert_equal nil, unit1.renter
   end
+
+  def test_it_can_add_renter
+    renter1 = Renter.new("Jessie")
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    unit1.add_renter(renter1)
+
+    assert_equal renter1, unit1.renter
+  end
+
 end
