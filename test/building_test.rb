b6c8a8780c8b4bb38928266fa55440abac8b26cf
PhilipDeFraties <philipjfry338@gmail.com>
2020-07-14 09:39:18 -0600

    Add test_it_exists test.

---
+++ test/building_test.rb
@@ -0,0 +1,16 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/renter'
+require './lib/apartment'
+require './lib/building'
+
+
+class BuildinbTest < MiniTest::Test
+
+  def test_it_exists
+    building = Building.new
+
+    assert_instance_of Building, building
+  end
+
+end
