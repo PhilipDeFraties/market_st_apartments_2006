5c9516c8be2fba55df2401ba3d2e54c1f0aed2e3
PhilipDeFraties <philipjfry338@gmail.com>
2020-07-14 09:28:25 -0600

    Fix placement of end of test class and rename file.

---
+++ test/apartment_test.rb
@@ -0,0 +1,43 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/renter'
+require './lib/apartment'
+
+class ApartmentTest < MiniTest::Test
+
+  def test_it_exists
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    assert_instance_of Apartment, unit1
+  end
+
+  def test_it_has_number
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    assert_equal "A1", unit1.number
+  end
+
+  def test_it_has_monthly_rent
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    assert_equal 1200, unit1.monthly_rent
+  end
+
+  def test_it_has_bathrooms
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    assert_equal 1, unit1.bedrooms
+  end
+
+  def test_it_has_bedrooms
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    assert_equal 1, unit1.bathrooms
+  end
+
+  def test_it_has_no_renter_by_default
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    assert_equal [], unit1.renter
+  end
+end
