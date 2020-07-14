09cd9c05f2a0372a0358838d93a6bae7ac798dae
PhilipDeFraties <philipjfry338@gmail.com>
2020-07-14 09:20:28 -0600

    Add test_it_has_bathrooms test.

--- test/apartment.rb
+++ test/apartment.rb
@@ -23,3 +23,9 @@ end
 
     assert_equal 1200, unit1.monthly_rent
   end
+
+  def test_it_has_bathrooms
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+
+    assert_equal 1, unit1.bedrooms
+  end
